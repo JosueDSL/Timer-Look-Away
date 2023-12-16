42deca5ef84645278e656a2fa980fef7cb6fe885
JosueDSL <josuedesantosl@gmail.com>
2023-12-15 23:25:01 -0600

    Added model db, create timers.html and its respective back-end logic

--- timerMain/timerApp/models.py
+++ timerMain/timerApp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Timer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="timers")
+    name = models.CharField(max_length=64)
+    interval = models.IntegerField()
+    message = models.CharField(max_length=128)
+    date = models.DateField(auto_now_add=True)
+   
+    def __str__(self):
+        return f"Name: {self.name} # Interval: {self.interval} # Date: {self.date}"
--- timerMain/timerApp/views.py
+++ timerMain/timerApp/views.py
@@ -6,8 +6,9 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Timer
 
+# Create your views here.
 def index(request):
     return render(request, 'timerApp/index.html')
 
@@ -33,25 +34,39 @@ def signup(request):
             return render(request, "timerApp/signup.html", {"message": "All fields must be filled out.."})
 
         # Check if username or email already exists
-        if User.objects.filter(username=username).exists():
+        elif User.objects.filter(username=username).exists():
             return render(request, "timerApp/signup.html", {"message": "Username already taken."})
 
         elif User.objects.filter(email=email).exists():
             return render(request, "timerApp/signup.html", {"message": "Email already in use."})
         
         # Check if username and password are valid
-        elif len(username) < 5:
-            return render(request, "timerApp/signup.html", {"message": "Username must be at least 5 characters."})
+        elif len(username) < 5 or len(username) > 64:
+            return render(request, "timerApp/signup.html", {"message": "Username must be between 5 and 64 characters."})
+        
         elif len(password) < 6:
             return render(request, "timerApp/signup.html", {"message": "Password must be at least 6 characters."})
         
         # Check if passwords match
-        if password != confirmation:
+        elif password != confirmation:
             return render(request, "timerApp/signup.html", {"message": "Passwords must match."})
 
         # Create new user
         user = User.objects.create_user(username, email, password)
+
+        # Add demo timer and intro message
+        message = f"Hello {user.username}! Welcome to Timer Look Away! This is a demo timer! Make the most of your time, thanks to Timer Look Away!"
+        if len(message) > 128:
+            message = f"Hello {user.username}! Welcome to Timer Look Away! Make the most of your time!"
+        
+        name = f"Demo Timer; Welcome {user.username} to TLA!"
+        if len(name) > 64:
+            name = f"Demo Timer, Welcome to TLA!"
+
+        add_demo = Timer.objects.create(user=user, name=name, interval=1, message=message)
+
         login(request, user)
+        
         return render(request, "timerApp/home.html", {"message": f"Thanks for joining {username.capitalize()}!, I hope you enjoy the app!"})
         
     else:
@@ -59,6 +74,7 @@ def signup(request):
 
 
 def login_view(request):
+
     if request.method == "POST":
 
         # Get form information
@@ -100,6 +116,60 @@ def home(request):
 
 @login_required(login_url='timerApp:login')   
 def timers(request):
+    
+    if request.method == "POST":
+        # Get form information
+        name = request.POST["timerName"]
+        minutes = request.POST["timerMinutes"]
+        hours = request.POST["timerHours"]
+        message = request.POST["timerSpeech"]
+        
+        # Get user information
+        user = request.user
+
+        # Get all timers associated with the user
+        timers = user.timers.all()
+
+        # Check if any fields are empty
+        if not name or not message or not minutes or not hours:
+            return render(request, "timerApp/timers.html", {"message": "All fields must be filled out."})
+        
+        # Check if timer name or speech message already exists
+        # Load conditional results into variables to avoid multiple database calls
+        name_exists = timers.filter(name=name).exists()
+        message_exists = timers.filter(message=message).exists()
+        
+        if name_exists or message_exists:
+            if name_exists:
+                return render(request, "timerApp/timers.html", {"message": "Timer name already exists."})
+            else:
+                return render(request, "timerApp/timers.html", {"message": "Speech message already exists."})
+
+        # Check if timer name and speech is valid
+        elif len(name) > 64 or len(message) > 128:
+            if len(name) > 64:
+                return render(request, "timerApp/timers.html", {"message": "Timer name must be less than 64 characters."})
+            else:
+                return render(request, "timerApp/timers.html", {"message": "Speech message must be less than 128 characters."})
+            
+        # Check if minutes and hours are valid
+        elif not minutes.isdigit() or not hours.isdigit():
+            return render(request, "timerApp/timers.html", {"message": "Minutes and hours must be numbers."})
+        
+        # Check if minutes and hours are within range
+        elif int(minutes) > 59 or int(hours) > 15:
+            return render(request, "timerApp/timers.html", {"message": "Minutes must be less than 60 and hours must be less than 16."})
+
+        # make sure timer is at least 1 minute
+        elif int(minutes) + int(hours) * 60 < 1:
+            return render(request, "timerApp/timers.html", {"message": "Timer must be at least 1 minute."})
+        
+        # Create new timer
+        set_timer = Timer.objects.create(user=user, name=name, interval=int(minutes) + int(hours) * 60, message=message)
+
+        return render(request, 'timerApp/home.html', {
+            "message": f"Timer {name} created!"})
+
     return render(request, 'timerApp/timers.html')
 
 @login_required(login_url='timerApp:login')
